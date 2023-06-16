a38f75cda6b930b09f38af31001ca4c98e712523
Englert, Fabian Emil <fabian.e.englert@stud.h-da.de>
2023-06-16 17:24:12 +0200

    begin path implementation

---
+++ path.go
@@ -0,0 +1,271 @@
+package quic
+
+import (
+	"github.com/quic-go/quic-go/logging"
+	"sync/atomic"
+	"time"
+
+	"github.com/quic-go/quic-go/internal/ackhandler"
+	"github.com/quic-go/quic-go/internal/congestion"
+	"github.com/quic-go/quic-go/internal/protocol"
+	"github.com/quic-go/quic-go/internal/qerr"
+	"github.com/quic-go/quic-go/internal/utils"
+	"github.com/quic-go/quic-go/internal/wire"
+)
+
+/*const (
+	minPathTimer = 10 * time.Millisecond
+	// XXX (QDC): To avoid idling...
+	maxPathTimer = 1 * time.Second
+)*/
+
+type path struct {
+	// Connection ID of the path
+	pathID protocol.ConnectionID
+	// Current connection which is the path runs on
+	conn connection
+
+	congestionSender congestion.SendAlgorithm
+
+	pathConn sendConn
+
+	rttStats *utils.RTTStats
+
+	sentPacketHandler     ackhandler.SentPacketHandler
+	receivedPacketHandler ackhandler.ReceivedMPPacketHandler
+
+	// Status if path is available or is on standby
+	status    atomic.Bool
+	closeChan chan *qerr.ApplicationError
+	runClosed chan struct{}
+
+	sentPacket chan struct{}
+
+	lastRcvdPacketNumber protocol.PacketNumber
+
+	// Used to calculate the next packet number from the truncated wire
+	// representation, and sent back in public reset packets
+	largestRcvdPacketNumber protocol.PacketNumber
+
+	leastUnacked protocol.PacketNumber
+
+	lastNetworkActivityTime time.Time
+
+	timer *utils.Timer
+}
+
+// setup initializes values that are independent of the perspective
+func (p *path) setup(perspective protocol.Perspective, tracer logging.ConnectionTracer) {
+	p.rttStats = &utils.RTTStats{}
+	//var cong congestion.SendAlgorithm
+
+	/*if p.conn.version >= protocol.Version1 && p.congestionSender != nil && p.pathID != p.conn.origDestConnID {
+		p.congestionSender = congestion.NewCubicSender(congestion.DefaultClock{}, p.rttStats, protocol.MaxPacketBufferSize, true, tracer)
+	}*/
+
+	now := time.Now()
+
+	p.sentPacketHandler, p.receivedPacketHandler = ackhandler.NewAckMPHandler(0, protocol.MaxPacketBufferSize, p.rttStats, true, 1, tracer, utils.DefaultLogger)
+
+	//p.packetNumberGenerator = p.sentPacketHandler.
+
+	p.closeChan = make(chan *qerr.ApplicationError, 1)
+	p.runClosed = make(chan struct{}, 1)
+	p.sentPacket = make(chan struct{}, 1)
+
+	p.timer = utils.NewTimer()
+	p.lastNetworkActivityTime = now
+
+	p.status.Store(true)
+	//p.potentiallyFailed.Store(false)
+
+	// Once the path is set up, run it
+	go p.run()
+}
+
+func (p *path) close() error {
+	p.status.Store(false)
+	return nil
+}
+
+func (p *path) run() {
+	// XXX (QDC): relay everything to the session, maybe not the most efficient
+runLoop:
+	for {
+		// Close immediately if requested
+		select {
+		case <-p.closeChan:
+			break runLoop
+		default:
+		}
+
+		p.maybeResetTimer()
+
+		select {
+		case <-p.closeChan:
+			break runLoop
+		case <-p.timer.Chan():
+			p.timer.SetRead()
+			select {
+			case p.conn.pathTimers <- p:
+			// XXX (QDC): don't remain stuck here!
+			case <-p.closeChan:
+				break runLoop
+			case <-p.sentPacket:
+				// Don't remain stuck here!
+			}
+		case <-p.sentPacket:
+			// Used to reset the path timer
+		}
+	}
+	err := p.close()
+	if err != nil {
+		return
+	}
+	p.sendQueue.Close()
+	p.timer.Stop()
+	p.runClosed <- struct{}{}
+}
+
+func (p *path) SendingAllowed() bool {
+	if p.sentPacketHandler.SendMode() != ackhandler.SendNone {
+		return p.status.Load()
+	}
+	return false
+}
+
+/*func (p *path) GetAckFrame() *wire.AckMPFrame {
+	ack := p.receivedPacketHandler.GetAckMPFrame()
+	if ack != nil {
+		ack.DestinationConnectionIDSequenceNumber = p.pathID
+	}
+
+	return ack
+}*/
+
+/*
+func (p *path) handlePathAbandonFrame() *wire.PathAbandonFrame {
+	return nil
+}
+*/
+
+/*
+func (p *path) handlePathStatusFrame() *wire.PathStatusFrame {
+	return nil
+}
+*/
+
+/*
+func (p *path) GetPathAbandonFrame() *wire.PathAbandonFrame {
+	pathAbandonFrame := p.receivedPacketHandler.GetClosePathFrame()
+	if closePathFrame != nil {
+		closePathFrame.PathID = p.pathID
+	}
+
+	return closePathFrame
+}*/
+
+func (p *path) idleTimeoutStartTime() time.Time {
+	return utils.MaxTime(p.lastNetworkActivityTime, p.conn.firstAckElicitingPacketAfterIdleSentTime)
+}
+
+func (p *path) maybeResetTimer() {
+	deadline := p.lastNetworkActivityTime.Add(p.idleTimeout())
+
+	if ackAlarm := p.receivedPacketHandler.GetAlarmTimeout(); !ackAlarm.IsZero() {
+		deadline = ackAlarm
+	}
+	if lossTime := p.receivedPacketHandler.GetAlarmTimeout(); !lossTime.IsZero() {
+		deadline = utils.MinTime(deadline, lossTime)
+	}
+
+	deadline = utils.MinTime(utils.MaxTime(deadline, time.Now().Add(10*time.Millisecond)), time.Now().Add(1*time.Second))
+
+	p.timer.Reset(deadline)
+}
+
+/*
+func (p *path) idleTimeout() time.Duration {
+	// TODO (QDC): probably this should be refined at path level
+	cryptoSetup := p.conn.cr
+	if cryptoSetup != nil {
+		if p.open.Load() && (p.pathID != 0 || p.conn.handshakeComplete) {
+			return p.conn.connectionParameters.GetIdleConnectionStateLifetime()
+		}
+		return p.conn.config.HandshakeTimeout
+	}
+	return time.Second
+}*/
+
+func (p *path) handlePacketImpl(pkt *receivedPacket) error {
+	if !p.status.Load() {
+		// Path is closed, ignore packet
+		return nil
+	}
+
+	if !pkt.rcvTime.IsZero() {
+		p.lastNetworkActivityTime = pkt.rcvTime
+	}
+	data := pkt.data
+	hdr, _, _, _ := wire.ParsePacket(data)
+
+	// We just received a new packet on that path, so it works
+	//p.potentiallyFailed.Store(false)
+	// Calculate packet number
+	hdr.PacketNumber = protocol.InferPacketNumber(
+		hdr.PacketNumberLen,
+		p.largestRcvdPacketNumber,
+		hdr.PacketNumber,
+	)
+
+	packet, err := p.conn.unpacker.UnpackLongHeader(hdr.Raw, hdr, data)
+	if utils.DefaultLogger.Debug() {
+		if err != nil {
+			utils.DefaultLogger.Debugf("<- Reading packet 0x%x (%d bytes) for connection %x on path %x", hdr.PacketNumber, len(data)+len(hdr.Raw), hdr.ConnectionID, p.pathID)
+		} else {
+			utils.DefaultLogger.Debugf("<- Reading packet 0x%x (%d bytes) for connection %x on path %x, %s", hdr.PacketNumber, len(data)+len(hdr.Raw), hdr.ConnectionID, p.pathID, packet.encryptionLevel)
+		}
+	}
+
+	// if the decryption failed, this might be a packet sent by an attacker
+	// don't update the remote address
+	if quicErr, ok := err.(*qerr.TransportErrorCode); ok && quicErr.String() == qerr.InternalError.String() {
+		return err
+	}
+	if p.conn.perspective == protocol.PerspectiveServer {
+		// update the remote address, even if unpacking failed for any other reason than a decryption error
+		p.conn.RemoteAddr()
+	}
+	if err != nil {
+		return err
+	}
+
+	p.lastRcvdPacketNumber = hdr.PacketNumber
+	// Only do this after decrupting, so we are sure the packet is not attacker-controlled
+	p.largestRcvdPacketNumber = utils.Max(p.largestRcvdPacketNumber, hdr.PacketNumber)
+
+	isRetransmittable := ackhandler.HasAckElicitingFrames(packet.data[0])
+	if err = p.receivedPacketHandler.ReceivedPacket(hdr.PacketNumber, isRetransmittable); err != nil {
+		return err
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return p.conn.handleFrames(packe, p)
+}
+
+func (p *path) onRTO(lastSentTime time.Time) bool {
+	// Was there any activity since last sent packet?
+	if p.lastNetworkActivityTime.Before(lastSentTime) {
+		//p.potentiallyFailed.Store(true)
+		p.conn.schedulePathsFrame()
+		return true
+	}
+	return false
+}
+
+func (p *path) SetLeastUnacked(leastUnacked protocol.PacketNumber) {
+	p.leastUnacked = leastUnacked
+}
---
+++ path_manager.go
@@ -0,0 +1,269 @@
+package quic
+
+
+import (
+	"errors"
+	"net"
+	"time"
+
+	"github.com/quic-go/quic-go/internal/congestion"
+	"github.com/quic-go/quic-go/internal/protocol"
+	"github.com/quic-go/quic-go/internal/utils"
+	"github.com/quic-go/quic-go/internal/wire"
+)
+
+type pathManager struct {
+	pconnMgr  *pconnManager
+	conn      *connection
+	nxtPathID protocol.ConnectionID
+	// Number of paths, excluding the initial one
+	nbPaths uint8
+
+	remoteAddrs4 []net.UDPAddr
+	remoteAddrs6 []net.UDPAddr
+
+	advertisedLocAddrs map[string]bool
+
+	// TODO (QDC): find a cleaner way
+	cubics map[protocol.ConnectionID]*congestion.Cubic
+
+	handshakeCompleted chan struct{}
+	runClosed          chan struct{}
+	timer              *time.Timer
+}
+
+func (pm *pathManager) setup(conn connection) {
+
+	connIDPath, err := protocol.GenerateConnectionIDForInitial()
+	pm.nxtPathID = connIDPath
+	if err != nil{
+		return
+	}
+
+	pm.remoteAddrs4 = make([]net.UDPAddr, 0)
+	pm.remoteAddrs6 = make([]net.UDPAddr, 0)
+	pm.advertisedLocAddrs = make(map[string]bool)
+	pm.handshakeCompleted = make(chan struct{}, 1)
+	pm.runClosed = make(chan struct{}, 1)
+	pm.timer = time.NewTimer(0)
+	pm.nbPaths = 0
+
+	pm.cubics := map[connIDPath]*congestion.OliaSender
+
+	// Set up the first path of the connection
+	pm.conn.paths[connIDPath] = &path{
+		pathID: pm.nxtPathID,
+		conn:   pm.conn,
+		pconn:   *conn,
+	}
+
+	// Setup this first path
+	pm.conn.paths[connIDPath].setup(pm.cubics)
+
+	// With the initial path, get the remoteAddr to create paths accordingly
+	if conn.RemoteAddr() != nil {
+		remAddr, err := net.ResolveUDPAddr("udp", conn.RemoteAddr().String())
+		if err != nil {
+			utils.DefaultLogger.Errorf("path manager: encountered error while parsing remote addr: %v", remAddr)
+		}
+
+		if remAddr.IP.To4() != nil {
+			pm.remoteAddrs4 = append(pm.remoteAddrs4, *remAddr)
+		} else {
+			pm.remoteAddrs6 = append(pm.remoteAddrs6, *remAddr)
+		}
+	}
+
+	// Launch the path manager
+	go pm.run()
+}
+
+func (pm *pathManager) run() {
+	// Close immediately if requested
+	select {
+	case <-pm.runClosed:
+		return
+	case <-pm.handshakeCompleted:
+		if pm.conn.multipath {
+			err := pm.createPaths()
+			if err != nil {
+				pm.closePaths()
+				return
+			}
+		}
+	}
+
+runLoop:
+	for {
+		select {
+		case <-pm.runClosed:
+			break runLoop
+		case <-pm.pconnMgr.changePaths:
+			if pm.conn.multipath {
+				pm.createPaths()
+			}
+		}
+	}
+	// Close paths
+	pm.closePaths()
+}
+
+func getIPVersion(ip net.IP) int {
+	if ip.To4() != nil {
+		return 4
+	}
+	return 6
+}
+
+func (pm *pathManager) advertiseAddresses() {
+	pm.pconnMgr.mutex.Lock()
+	defer pm.pconnMgr.mutex.Unlock()
+	for _, locAddr := range pm.pconnMgr.localAddrs {
+		_, sent := pm.advertisedLocAddrs[locAddr.String()]
+		if !sent {
+			version := getIPVersion(locAddr.IP)
+			pm.conn.framer.AddAddressForTransmission(uint8(version), locAddr)
+			pm.advertisedLocAddrs[locAddr.String()] = true
+		}
+	}
+}
+
+func (pm *pathManager) createPath(locAddr net.UDPAddr, remAddr net.UDPAddr) error {
+	// First check that the path does not exist yet
+	pm.conn.pathMutex.Lock()
+	defer pm.conn.pathMutex.Unlock()
+	paths := pm.conn.paths
+	for _, pth := range paths {
+		locAddrPath := pth.conn.LocalAddr().String()
+		remAddrPath := pth.conn.RemoteAddr().String()
+		if locAddr.String() == locAddrPath && remAddr.String() == remAddrPath {
+			// Path already exists, so don't create it again
+			return nil
+		}
+	}
+	// No matching path, so create it
+	pth := &path{
+		pathID: pm.nxtPathID,
+		conn:   pm.conn,
+		pconn:   &conn{pconn: pm.pconnMgr.pconns[locAddr.String()], currentAddr: &remAddr},
+	}
+	pth.setup(pm.cubics)
+	pm.conn.paths[pm.nxtPathID] = pth
+	if utils.Debug() {
+		utils.Debugf("Created path %x on %s to %s", pm.nxtPathID, locAddr.String(), remAddr.String())
+	}
+	nxtPathID, err := protocol.GenerateConnectionID(10)
+	pm.nxtPathID = nxtPathID
+	if err != nil{
+		return err
+	}
+	// Send a PING frame to get latency info about the new path and informing the
+	// peer of its existence
+	// Because we hold pathsLock, it is safe to send packet now
+	return pm.conn.sendPing(pth)
+}
+
+func (pm *pathManager) createPaths() error {
+	if utils.Debug() {
+		utils.Debugf("Path manager tries to create paths")
+	}
+
+	// XXX (QDC): don't let the server create paths for now
+	if pm.conn.perspective == protocol.PerspectiveServer {
+		pm.advertiseAddresses()
+		return nil
+	}
+	// TODO (QDC): clearly not optimal
+	pm.pconnMgr.mutex.Lock()
+	defer pm.pconnMgr.mutex.Unlock()
+	for _, locAddr := range pm.pconnMgr.localAddrs {
+		version := getIPVersion(locAddr.IP)
+		if version == 4 {
+			for _, remAddr := range pm.remoteAddrs4 {
+				err := pm.createPath(locAddr, remAddr)
+				if err != nil {
+					return err
+				}
+			}
+		} else {
+			for _, remAddr := range pm.remoteAddrs6 {
+				err := pm.createPath(locAddr, remAddr)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	pm.conn.schedulePathsFrame()
+	return nil
+}
+
+func (pm *pathManager) createPathFromRemote(p *receivedPacket) (*path, error) {
+	pm.conn.pathMutex.Lock()
+	defer pm.conn.pathMutex.Unlock()
+	localPconn := p.rcvPconn
+	remoteAddr := p.remoteAddr
+	pathID := p.publicHeader.PathID
+
+	// Sanity check: pathID should not exist yet
+	_, ko := pm.conn.paths[pathID]
+	if ko {
+		return nil, errors.New("trying to create already existing path")
+	}
+
+	// Sanity check: odd is client initiated, even for server initiated
+	if pm.conn.perspective == protocol.PerspectiveClient && pathID%2 != 0 {
+		return nil, errors.New("server tries to create odd pathID")
+	}
+	if pm.conn.perspective == protocol.PerspectiveServer && pathID%2 == 0 {
+		return nil, errors.New("client tries to create even pathID")
+	}
+
+	pth := &path{
+		pathID: pathID,
+		sess:   pm.conn,
+		conn:   &conn{pconn: localPconn, currentAddr: remoteAddr},
+	}
+
+	pth.setup(pm.cubics)
+	pm.conn.paths[pathID] = pth
+
+	if utils.Debug() {
+		utils.Debugf("Created remote path %x on %s to %s", pathID, localPconn.LocalAddr().String(), remoteAddr.String())
+	}
+
+	return pth, nil
+}
+
+func (pm *pathManager) closePath(pthID protocol.ConnectionID) error {
+	pm.conn.pathMutex.RLock()
+	defer pm.conn.pathMutex.RUnlock()
+
+	pth, ok := pm.conn.paths[pthID]
+	if !ok {
+		// XXX (QDC) Unknown path, what should we do?
+		return nil
+	}
+
+	if pth.status.Load() {
+		pth.closeChan <- nil
+	}
+
+	return nil
+}
+
+func (pm *pathManager) closePaths() {
+	pm.conn.pathMutex.RLock()
+	paths := pm.conn.paths
+	for _, pth := range paths {
+		if pth.status.Load() {
+			select {
+			case pth.closeChan <- nil:
+			default:
+				// Don't remain stuck here!
+			}
+		}
+	}
+	pm.conn.pathMutex.RUnlock()
+}
+
