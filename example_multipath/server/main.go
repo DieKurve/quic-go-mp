0db844246c7897de76085552efd350369a9d33ab
Englert, Fabian Emil <fabian.e.englert@stud.h-da.de>
2023-06-27 10:16:57 +0200

    changed address to localhost for now

--- example_multipath/server/main.go
+++ example_multipath/server/main.go
@@ -1,78 +1,77 @@
 package main
 
 import (
-  "context"
-  "crypto/rand"
-  "crypto/rsa"
-  "crypto/tls"
-  "crypto/x509"
-  "encoding/pem"
-  "fmt"
-  "github.com/quic-go/quic-go"
-  "io"
-  "math/big"
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"github.com/quic-go/quic-go"
+	"io"
+	"math/big"
 )
 
-const addr1 = "192.168.178.112:10033"
-const message = "foobar"
+const addrServer = "localhost:1337"
 
 // We start a server echoing data on the first stream the client opens,
 // then connect with a client, send the message, and wait for its receipt.
 func main() {
-  err := echoServer()
-  if err != nil {
-    panic(err)
-  }
+	err := echoServer()
+	if err != nil {
+		panic(err)
+	}
 }
 
 // Start a server that echos all data on the first stream opened by the client
 func echoServer() error {
-  listener, err := quic.ListenAddr(addr1, generateTLSConfig(), nil, 1)
-  if err != nil {
-    return err
-  }
-  conn, err := listener.Accept(context.Background())
-  if err != nil {
-    return err
-  }
-  stream, err := conn.AcceptStream(context.Background())
-  if err != nil {
-    panic(err)
-  }
-  // Echo through the loggingWriter
-  _, err = io.Copy(loggingWriter{stream}, stream)
-  return err
+	listener, err := quic.ListenAddr(addrServer, generateTLSConfig(), nil, 1)
+	if err != nil {
+		return err
+	}
+	conn, err := listener.Accept(context.Background())
+	if err != nil {
+		return err
+	}
+	stream, err := conn.AcceptStream(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	// Echo through the loggingWriter
+	fmt.Print(conn.RemoteAddr().String() + ": ")
+	_, err = io.Copy(loggingWriter{stream}, stream)
+	return err
 }
 
 // A wrapper for io.Writer that also logs the message.
 type loggingWriter struct{ io.Writer }
 
 func (w loggingWriter) Write(b []byte) (int, error) {
-  fmt.Printf("Server: Got '%s'\n", string(b))
-  return w.Writer.Write(b)
+	fmt.Printf("Got '%s'\n", string(b))
+	return w.Writer.Write(b)
 }
-
 
 // Set up a bare-bones TLS config for the server
 func generateTLSConfig() *tls.Config {
-  key, err := rsa.GenerateKey(rand.Reader, 1024)
-  if err != nil {
-    panic(err)
-  }
-  template := x509.Certificate{SerialNumber: big.NewInt(1)}
-  certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-  if err != nil {
-    panic(err)
-  }
-  keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-  certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
 
-  tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-  if err != nil {
-    panic(err)
-  }
-  return &tls.Config{
-    Certificates: []tls.Certificate{tlsCert},
-    NextProtos:   []string{"quic-echo-example"},
-  }
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{tlsCert},
+		NextProtos:   []string{"quic-echo-example"},
+	}
 }
