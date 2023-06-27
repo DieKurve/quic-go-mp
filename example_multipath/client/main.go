ca399c0b8482b4c2112b65af257769fbe0a3e056
Englert, Fabian Emil <fabian.e.englert@stud.h-da.de>
2023-06-27 08:56:26 +0200

    Added wlan as possible interface and removed tls function and logging

--- example_multipath/client/main.go
+++ example_multipath/client/main.go
@@ -1,103 +1,95 @@
 package main
 
 import (
-  "context"
-  "crypto/rand"
-  "crypto/rsa"
-  "crypto/tls"
-  "crypto/x509"
-  "encoding/pem"
-  "fmt"
-  "github.com/quic-go/quic-go"
-  "github.com/quic-go/quic-go/internal/utils"
-  "io"
-  "math/big"
-  "net"
-  "strings"
+	"context"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"github.com/quic-go/quic-go"
+	"github.com/quic-go/quic-go/internal/utils"
+	"io"
+	"net"
+	"os"
+	"strings"
 )
 
 const message = "foobar"
 
-func getAddresses() []string{
-  addresses := make([]string, 0)
-  interfaceNet, _ := net.Interfaces()
-  interfaceLoop:
-  for _, currentInterface := range interfaceNet {
-    if !strings.Contains(currentInterface.Name, "enp") {
-      continue
-    }
-    address, err := currentInterface.Addrs()
-    if err != nil {
-      panic(err)
-    }
-    for _, a := range address {
-      ip, _, err := net.ParseCIDR(a.String())
-      if err != nil {
-        panic(err)
-      }
-      if utils.IsIPv4(ip){
-        fmt.Println("Added " + ip.String())
-        addresses = append(addresses, ip.String()+":10033")
-        continue interfaceLoop
-      }
-    }
-  }
-  return addresses
+func getLocalAddresses() []string {
+	addresses := make([]string, 0)
+	interfaceNet, _ := net.Interfaces()
+interfaceLoop:
+	for _, currentInterface := range interfaceNet {
+		if !strings.Contains(currentInterface.Name, "en") && !strings.Contains(currentInterface.Name, "wlp") {
+			continue
+		}
+		address, err := currentInterface.Addrs()
+		if err != nil {
+			panic(err)
+		}
+		for _, a := range address {
+			ip, _, err := net.ParseCIDR(a.String())
+			if err != nil {
+				panic(err)
+			}
+			if utils.IsIPv4(ip) {
+				fmt.Println("Added " + ip.String())
+				addresses = append(addresses, ip.String())
+				continue interfaceLoop
+			}
+		}
+	}
+	return addresses
 }
 
 // We start a server echoing data on the first stream the client opens,
 // then connect with a client, send the message, and wait for its receipt.
 func main() {
-  err := clientMain()
-  if err != nil {
-    panic(err)
-  }
+	err := clientMain()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
-
 
 func clientMain() error {
-  tlsConf := &tls.Config{
-    InsecureSkipVerify: true,
-    NextProtos:         []string{"quic-echo-example"},
-  }
-  addresses := getAddresses()
-  conn, err := quic.DialAddr(addresses[0], tlsConf, nil, 1)
-  if err != nil {
-    return err
-  }
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo-example"},
+	}
+	if len(os.Args) <= 1 {
+		return errors.New("no server address given as parameter")
+	}
+	serverAddress := os.Args[1] + ":1337"
+	addresses := getLocalAddresses()
+	conn, err := quic.DialAddr(serverAddress, tlsConf, nil, 1)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < len(addresses); i++ {
+		err = conn.AddPath(addresses[i])
+		if err != nil {
+			return err
+		}
+	}
 
-  for i := 1; i < len(addresses); i++ {
-    err = conn.AddPath(addresses[i])
-    if err != nil {
-      return err
-    }
-  }
+	stream, err := conn.OpenStreamSync(context.Background())
+	if err != nil {
+		return err
+	}
 
-  stream, err := conn.OpenStreamSync(context.Background())
-  if err != nil {
-    return err
-  }
+	fmt.Printf("Client: Sending '%s' on %s \n", message, conn.LocalAddr().String())
+	_, err = stream.Write([]byte(message))
+	if err != nil {
+		return err
+	}
 
-  fmt.Printf("Client: Sending '%s'\n", message)
-  _, err = stream.Write([]byte(message))
-  if err != nil {
-    return err
-  }
+	buf := make([]byte, len(message))
+	_, err = io.ReadFull(stream, buf)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Client: Got '%s' from %s \n", buf, conn.RemoteAddr().String())
 
-  buf := make([]byte, len(message))
-  _, err = io.ReadFull(stream, buf)
-  if err != nil {
-    return err
-  }
-  fmt.Printf("Client: Got '%s'\n", buf)
-
-  return nil
-}
-
-// A wrapper for io.Writer that also logs the message.
-type loggingWriter struct{ io.Writer }
-
-func (w loggingWriter) Write(b []byte) (int, error) {
-  fmt.Printf("Server: Got '%s'\n", string(b))
-  return w.Writer.Write(b)
+	return nil
 }
