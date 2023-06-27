742a12955424f03230a5c54e2893329f0317e936
Englert, Fabian Emil <fabian.e.englert@stud.h-da.de>
2023-06-27 07:38:37 +0200

    Removed tls set up for server

--- example_multipath/client/main.go
+++ example_multipath/client/main.go
@@ -101,28 +101,3 @@ func (w loggingWriter) Write(b []byte) (int, error) {
   fmt.Printf("Server: Got '%s'\n", string(b))
   return w.Writer.Write(b)
 }
-
-
-// Set up a bare-bones TLS config for the server
-func generateTLSConfig() *tls.Config {
-  key, err := rsa.GenerateKey(rand.Reader, 1024)
-  if err != nil {
-    panic(err)
-  }
-  template := x509.Certificate{SerialNumber: big.NewInt(1)}
-  certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-  if err != nil {
-    panic(err)
-  }
-  keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-  certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-  tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-  if err != nil {
-    panic(err)
-  }
-  return &tls.Config{
-    Certificates: []tls.Certificate{tlsCert},
-    NextProtos:   []string{"quic-echo-example"},
-  }
-}
